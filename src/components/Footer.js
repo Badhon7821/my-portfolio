06e8d5aa3de0af14df5319cc0ea94b544e489de7
Badhon7821 <bayozidbostamibadhon@gmail.com>
2023-02-06 10:57:24 -0800

    footer section complete

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,21 @@
+import { HashLink } from "react-router-hash-link";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="text-center py-32 text-white/50 overflow-hidden">
+      <p>
+        &copy; {new Date().getFullYear()}
+        <HashLink
+          smooth
+          to="#home"
+          className="link-item hover:text-white duration-500"
+        >
+          Badhon
+        </HashLink>
+      </p>
+      created by yours truly
+    </footer>
+  );
 };
 
 export default Footer;
