2f157f8ade98869b1ff52e8ff432dc52be6a4c08
Badhon7821 <bayozidbostamibadhon@gmail.com>
2023-02-07 01:44:05 -0800

    smooth scrool hook created

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,5 +1,21 @@
+const data = [
+  { id: 1, title: "Facebook", url: "https://www.google.com" },
+  { id: 2, title: "Twitter", url: "https://www.google.com" },
+  { id: 3, title: "GitHub", url: "https://www.google.com" },
+  { id: 4, title: "LinkedIn", url: "https://www.google.com" },
+  { id: 5, title: "Leetcode", url: "https://www.google.com" },
+];
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="socials flex gap-5 text-sm uppercase -rotate-90 fixed left-10 top-[75%] origin-left text-white/30 bg-black/30 p-5 backdrop-blur-xl z-50">
+      {data.map((social) => (
+        <a className="hover:text-cyan-400 duration-500" href={social.url}>
+          {social.title}
+        </a>
+      ))}
+    </div>
+  );
 };
 
 export default Socials;
