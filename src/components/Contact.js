bc028c2e7eeeab5d5190019bb2873268dcc3136a
Badhon7821 <bayozidbostamibadhon@gmail.com>
2023-02-07 00:41:00 -0800

    about section design complete

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,11 @@
+import SectionTitle from "./SectionTitle";
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="container mx-auto mt-40">
+      <SectionTitle title={"Contact"} />
+    </div>
+  );
 };
 
 export default Contact;
