564448553686b8bcb483cc3dc9dad731f9aba76d
Badhon7821 <bayozidbostamibadhon@gmail.com>
2023-02-09 01:30:02 -0800

    done

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -22,14 +22,14 @@ const data = [
   },
   {
     id: 2,
-    title: "FOOD VERSE — AN FOOD STORE",
-    img1: "https://res.cloudinary.com/dpkbthpcw/image/upload/v1675265034/Personal%20Portfolio%202/shopping-1_nx6sfr.jpg",
+    title: "IMMEMORIAL — AN ETHICAL CANVAS",
+    img1: "https://res.cloudinary.com/deg5bflae/image/upload/v1675934371/my-portfolio/Screenshot_1_pxwouz.png",
     img2: "https://res.cloudinary.com/dpkbthpcw/image/upload/v1675265034/Personal%20Portfolio%202/shopping-2_z3boxd.jpg",
     description:
-      "Recipe functionality has been implemented using React. All the product data has been served via JSON server and fetched. Cart items also saved into the local storage of the client’s browser and add favorite cart and view this favourites page",
-    tools: ["React.js", "Tailwind CSS"],
-    liveLink: "https://deluxe-souffle-9a6977.netlify.app/favourites",
-    frontEndLink: "https://www.google.com",
+      "immemorial website is fully animated website. This website created by React, animation by gsap animation and style by css framework tailwind css",
+    tools: ["React.js", "Tailwind CSS", "gsap"],
+    liveLink: "https://classy-selkie-de8933.netlify.app/",
+    frontEndLink: "https://github.com/Badhon7821/immemorial-gsap-animation",
   },
 ];
 
